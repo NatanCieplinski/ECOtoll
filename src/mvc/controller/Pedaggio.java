0bae57334c659f623f026191f3912e24ea8d1e39
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2019-10-13 18:49:28 +0200

    Aggiunta della classe pedaggio

---
+++ src/mvc/controller/Pedaggio.java
@@ -0,0 +1,9 @@
+package mvc.controller;
+
+import mvc.model.Veicolo;
+
+public class Pedaggio{
+    public static float calcoloPedaggio(int idCaselloIngresso, Veicolo veicolo, int diCaselloUscita){
+        return (float)0.0;
+    }
+}
