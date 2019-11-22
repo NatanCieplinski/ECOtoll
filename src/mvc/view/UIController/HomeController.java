19a490343f64326d1e97cf40b1e5109e40c8df07
Patrizia Villani <patrizia.vil@icloud.com>
2019-11-22 16:02:08 +0100

    Interfaccia ultimata

--- src/mvc/view/UIController/HomeController.java
+++ src/mvc/view/UIController/HomeController.java
@@ -1,7 +1,7 @@
+
 package mvc.view.UIController;
 
 import java.net.URL;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,7 +9,6 @@ import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import dao.database.DBManager;
 import dao.exceptions.DBException;
 import dao.implementation.AutostradaDao;
 import dao.implementation.BigliettoDao;
@@ -20,7 +19,6 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -42,27 +40,18 @@ import mvc.model.Veicolo;
 
 
 
-
 import javafx.scene.control.ToggleGroup;
 
 public class HomeController implements Initializable {
 
 	// VARIABILI DI CALCOLO
 	
-	private DBManager dbFactory = DBManager.getDaoFactory(DBManager.MYSQL);
-	
-	
-	
-	
-	private Autostrada autostradaSelezionata = null; // id dell'autostrada selezionata
+	private Autostrada autostradaSelezionata = null; 
 	private Casello caselloSelezionato = null; 
-	private Integer veicoloSelezionato = null;
-	private String targaSelezionata = "";
-	private Integer io = null; // id ingresso-uscita
+	private Integer io = null; 
 	private Integer ud = null;
 	private boolean sn;
-	private Casello CaselloNuovo = null;
-	
+
 	static ObservableList<String> items = FXCollections.observableArrayList();
 	static LinkedList<String> targhe = new LinkedList<String>();
 	
@@ -216,9 +205,6 @@ public class HomeController implements Initializable {
 		
 		AutostradaDao el = new AutostradaDao();
 	    LinkedList<Autostrada> autostradaList = null;
-		
-	//    MenuButtonSettoreCasello.setText("Selettore Autostrada");
-	//    MenuButtonSettoreCasello.setText(caselloSelezionato.getNome(). ) ;
 	    
 	    try {
 	    	autostradaList = (LinkedList<Autostrada>)el.getAll();
@@ -233,11 +219,11 @@ public class HomeController implements Initializable {
 	    	// evento di click sul MenuItem
 	    	prov.setOnAction(new EventHandler<ActionEvent>() {
 	    		
-
 				@Override
 				public void handle(ActionEvent event) {
+					
 					autostradaSelezionata = a;
-					System.out.println(autostradaSelezionata);
+					System.out.println(autostradaSelezionata.getId());
 					setMenuItemsCaselli(0);
 					MenuButtonSettoreAutostrada.setText(autostradaSelezionata.getNome());
 					
@@ -245,34 +231,41 @@ public class HomeController implements Initializable {
 				
 	    	});
 	    	
-	    	
 	    	MenuButtonSettoreAutostrada.getItems().add(prov);
-	    //	 MenuButtonSettoreAutostrada.setText(autostradaSelezionata.getNome());
+	    
 	    }
+	    
 	    
 	    // listener 
 	    TFcerca.textProperty().addListener(new ChangeListener<String>() {
+	    	
 	    	@Override
 	    	public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-	    		//System.out.println(" Text Changed to " + newValue + ")\n");
+	    		
 	    		ObservableList<String> prov = FXCollections.observableArrayList();;
 	    		String input = TFcerca.getText();
-	    		//System.out.println(input);
+	    		System.out.println(input);
 	    		input = "^" + input + "[\\w]*\\s*[\\d]*\\s*[\\w]*";
-	    		//System.out.println(input);
+	    		System.out.println(input);
 	    		Pattern pattern = Pattern.compile(input);
+	    		
 	    		for (String s : targhe) {
-	    			//System.out.println("sto valutando " +  s);
+	    			System.out.println("sto valutando " +  s);
 	    			Matcher m = pattern.matcher(s);
 	    			Tab.getItems().clear();
-	    			//System.out.println("k");
+	    			System.out.println("k");
+	    			
 	    			while(m.find()) {
-	    				//System.out.println("stringa" + m.group());
+	    				System.out.println("stringa" + m.group());
 	    				prov.add(m.group());
 	    			}
+	    			
 	    		}
+	    		
 	    		Tab.setItems(prov);
+	    		
 	    	}
+	    	
 	    });
 		
 	}
@@ -283,8 +276,13 @@ public class HomeController implements Initializable {
 
 		if (autostradaSelezionata != null)
 			MenuButtonSettoreCasello.setDisable(false);
+			MenuButtonSettoreCaselloModifica.setDisable(false);
+			MenuButtonSettoreCaselloAggRim.setDisable(false);
+	
 		MenuButtonSettoreCasello.getItems().clear();
-
+		MenuButtonSettoreCaselloModifica.getItems().clear();
+		MenuButtonSettoreCaselloAggRim.getItems().clear();
+		
 		CaselloDao il = new CaselloDao();
 		AutostradaDao daoAuto = new AutostradaDao();
 		LinkedList<Casello> caselloList = null;
@@ -337,16 +335,20 @@ public class HomeController implements Initializable {
 		}
 
 	}
-	 @FXML
-	    void Cancella(MouseEvent event) {
-		 	MenuButtonSettoreCasello.setText("Selettore Autostrada");
-	    }
-
+	
+	@FXML
+	void Cancella(MouseEvent event) {
+		 
+		MenuButtonSettoreCasello.setText("Selettore Autostrada");
+		 	
+	}
+	 
 	@FXML
 	public void EmettiBIglietto(MouseEvent event) {
 
 		targa = TFcerca.getText();
 		System.out.println(targa);
+		targa = Tab.getSelectionModel().getSelectedItem();
 
 		if (io == 0) {
 
@@ -355,22 +357,23 @@ public class HomeController implements Initializable {
 		} else {
 
 			if (io == null || ud == null || targa == null) { 
+				
 				System.out.println("compila tutti i campi");
+				
 			} else {
 
 				GestoreAutostradale ga = new GestoreAutostradale();
 				ga.ingresso(targa, caselloSelezionato.getId(), sn, ud);
+				
 			}
 
 		}
-
+		
 	}
 
 	@FXML
 	void clickIngresso(MouseEvent event) throws DBException, SQLException {
 		io = 1;
-
-
 		
 		RdDue.setDisable(false);
 		RdUno.setDisable(false);
@@ -381,29 +384,22 @@ public class HomeController implements Initializable {
 		RdIngresso.setToggleGroup(radioGroup2);
 		RdUscita.setToggleGroup(radioGroup2);
 
-		
 		// prendiamo le automobili che potrebbero entrare nell'autostrada
 		
 		VeicoloDao v = new VeicoloDao();
-		
 		List<Veicolo> listaAuto = (List<Veicolo>)v.getAll();
-
 		targhe.clear();
 		
     	for (Veicolo ve : listaAuto) {
     		targhe.add(ve.getTarga());
     	}
+    	
     	Tab.getItems().clear();
+    	
     	for(String targa: targhe) {
     		Tab.getItems().add(targa);
     	}
  
-
-	}
-
-	@FXML
-	void Pagamento(MouseEvent event) {
-
 	}
 
 	@FXML
@@ -414,6 +410,7 @@ public class HomeController implements Initializable {
 		ToggleGroup radioGroup3 = new ToggleGroup();
 		RdSi.setToggleGroup(radioGroup3);
 		RdNo.setToggleGroup(radioGroup3);
+		
 	}
 
 	@FXML
@@ -455,10 +452,13 @@ public class HomeController implements Initializable {
 		BigliettoDao b = new BigliettoDao();
 		List<Biglietto> listaBiglietti = (List<Biglietto>)b.getAll(); // chiamata al metodo getAll();
 		targhe.clear();
+		
     	for (Biglietto bi : listaBiglietti) {
     		targhe.add(bi.getTarga());
     	}
+    	
     	Tab.getItems().clear();
+    	
     	for(String targa: targhe) {
     		Tab.getItems().add(targa);
     	}
@@ -470,41 +470,52 @@ public class HomeController implements Initializable {
 
 		float prezzo = 0;
 
-		this.targa = TFcerca.getText();
-		
 		GestoreAutostradale au = new GestoreAutostradale();
 		prezzo = au.calcoloPrezzo(targa, caselloSelezionato.getId());
 
 		labelPr.setText(prezzo + " €");
-		// pannelloBase.setDisable(true);
-		// pannelloPedaggio.setVisible(true);
-
-		// labelPr.setText(au);
-
+		
 	}
 
 	@FXML
 	void TFcerca(ActionEvent event) {
 		  		 
-
 	}
 	
-	 	
-	 	
 	 	 @FXML
 	     void ClickIndietro(MouseEvent event) {
 	 		pannelloModificaCasello.setVisible(false);
+	    	MenuButtonSettoreCasello.setText("Selettore Casello");
+
 	     }
 
+	 	@FXML
+	    void SelAutMod(MouseEvent event) {
+	 		
+	 		 MenuButtonSettoreCaselloModifica.setText("Selettore Casello");		//
+	 		 MenuButtonSettoreCaselloModifica.getItems().clear();	
+
+	    }
+	 	
+	 	@FXML
+	    void SelAutMod2(MouseEvent event) {
+	 		
+	 		MenuButtonSettoreCaselloAggRim.setText("Selettore Casello");	
+	 		
+	    }
 
 	    @FXML
 	    void ClickModifica(MouseEvent event) throws DBException, SQLException {
 
 			pannelloModificaCasello.setVisible(true);
 			
-			
-			
-			
+			 MenuButtonSettoreAutostradaModifica.setText("Selettore Autostrada");
+			 MenuButtonSettoreAutostradaModifica.getItems().clear();
+			 nomeCaselloModifica.clear();
+			 chilometroCaselloModifica.clear();
+			 MenuButtonSettoreCaselloModifica.getItems().clear();
+			 MenuButtonSettoreCaselloModifica.setText("Selettore Casello");
+
 			AutostradaDao el = new AutostradaDao();
 		    LinkedList<Autostrada> autostradaList = null;
 			
@@ -529,6 +540,7 @@ public class HomeController implements Initializable {
 						setMenuItemsCaselli(1);
 						MenuButtonSettoreAutostradaModifica.setText(autostradaSelezionata.getNome());
 					}
+					
 		    	});
 
 		    	MenuButtonSettoreAutostradaModifica.getItems().add(prov);
@@ -539,19 +551,21 @@ public class HomeController implements Initializable {
 	    }
 	    
 	    
-	    @FXML
-	    void SalvaModifiches(MouseEvent event) throws DBException, SQLException {
+	@FXML
+	void SalvaModifiche(MouseEvent event) throws DBException, SQLException {
     
-		    CaselloDao dao = new CaselloDao();
-		    String[] params = new String[3];
-		//    params[0] = Integer.toString(autostradaSelezionata);
-		    params[1] = nomeCaselloModifica.getText();
-		    params[2] = chilometroCaselloModifica.getText();
-		    dao.update(caselloSelezionato, params);
+	    CaselloDao dao = new CaselloDao();
+		String[] params = new String[3];
+		params[1] = nomeCaselloModifica.getText();
+		params[2] = chilometroCaselloModifica.getText();
+		dao.update(caselloSelezionato, params);
+		    
+		MenuButtonSettoreAutostradaModifica.setText("Selettore Autostrada");
+		MenuButtonSettoreCaselloModifica.setText("Selettore Casello");
+		nomeCaselloModifica.clear();
+		chilometroCaselloModifica.clear();
 			
 	    }
-
-	    
 	    
 	    @FXML
 	    void ClickRimuovi(MouseEvent event) throws DBException, SQLException {
@@ -560,12 +574,25 @@ public class HomeController implements Initializable {
 	    	
 	    	dao.delete(caselloSelezionato);
 	    	
+	    	MenuButtonSettoreAutostradaRimuovi.setText("Selettore Autostrada");
+	    	MenuButtonSettoreCaselloAggRim.setText("Selettore Casello");
+
 	    }
 	    
 	    @FXML
 	    void ClickAggRim(MouseEvent event) {
 	    	
 	    	PannelloAggRim.setVisible(true);
+	    	
+	    	MenuButtonSettoreAutostradaRimuovi.setText("Selettore Autostrada");
+	    	MenuButtonSettoreAutostradaRimuovi.getItems().clear();
+	    	MenuButtonSettoreAutostradaAggiungi.setText("Selettore Autostrada");
+	    	MenuButtonSettoreAutostradaAggiungi.getItems().clear(); 
+	    	MenuButtonSettoreCaselloAggRim.getItems().clear();
+			MenuButtonSettoreCaselloAggRim.setText("Selettore Casello");
+
+			CaselloAggiunto.clear();
+			ChilometroAggiunto.clear();
 	    	
 	    	var = 2;
 
@@ -610,30 +637,24 @@ public class HomeController implements Initializable {
 		    	
 		    	MenuButtonSettoreAutostradaRimuovi.getItems().add(prov);
 		    	MenuButtonSettoreAutostradaAggiungi.getItems().add(prov2);
-		    	
-		    	 	
+		    		 	
 		    }	
 	    	
 	    }
-	    
 
 	    @FXML
 	    void ClickIndietro2(MouseEvent event) {
 	    	
 	    	PannelloAggRim.setVisible(false);
+	    	MenuButtonSettoreCasello.setText("Selettore Casello");
+
 	    	
 	    }
-	    @FXML
-	    void Sole(MouseEvent event) {
 
-	    	idCaselloAggiunta = 1;
-		
-	    }
 	    
 	    @FXML
 	    void ClickAggiungi(MouseEvent event) throws DBException, SQLException {
 	    	
-
 		    CaselloDao dao = new CaselloDao();
 		    String[] params = new String[3];
 		    params[0] = Integer.toString(autostradaSelezionata.getId());
@@ -644,10 +665,11 @@ public class HomeController implements Initializable {
 		    params[2] = ChilometroAggiunto.getText();
 		    dao.create(params);
 		    
-	//	    CaselloAggiunto.get
+		    CaselloAggiunto.clear();
+		    ChilometroAggiunto.clear();
+		    MenuButtonSettoreAutostradaAggiungi.setText("Selettore Autostrada");
 
 	    }
-		
-		
-
+	    
+	 
 }
