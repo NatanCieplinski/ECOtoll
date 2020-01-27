4e2eb5f82c78555e3451a7bcb21f3074e7d9cbfa
Alessandro Lodi <alelod96@gmail.com>
2020-01-27 16:06:40 +0100

    Fix prezzo

--- src/mvc/controller/Pedaggio.java
+++ src/mvc/controller/Pedaggio.java
@@ -7,12 +7,8 @@ public class Pedaggio{
     public static float calcoloPedaggio(Veicolo veicolo, Casello caselloIngresso, Casello caselloUscita, Normativa normativa){
         int km = Math.abs(caselloIngresso.getChilometro() - caselloUscita.getChilometro());
         
-        float pedaggio = normativa.round(
-        	veicolo.getTariffa(caselloUscita) * 
-        	km * 
-        	normativa.getIva()
-        );
+        float pedaggio = veicolo.getTariffa(caselloUscita) * km * normativa.getIva();
         
-        return normativa.differenziazione(pedaggio, veicolo);
+        return normativa.round(normativa.differenziazione(pedaggio, veicolo));
     }
 }
